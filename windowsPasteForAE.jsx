cd5a4d59711c7392b27f71a768a5eeb11d78a13c
qdozaq <doza@indiginis.com>
2017-08-15 02:37:08 -0400

    working draft

---
+++ windowsPasteForAE.jsx
@@ -0,0 +1,30 @@
+﻿var win = new Window("palette", "paste", undefined);
+
+// win.panel = win.add("panel", undefined, "Paste");
+win.btn = win.add("Button", undefined, "Paste");
+win.textBox = win.add("edittext", [0,0,100,100], "editme",{multiline:true, scrollable:true});
+
+win.btn.onClick = function() {
+    app.beginUndoGroup("paste");
+    paste();
+    app.endUndoGroup();
+};
+
+win.show();
+
+function paste() {
+    var mainComp = app.project.activeItem;
+    if (mainComp == null) {
+        // alert("please select a text layer");
+        return;
+    }
+
+    var selected = mainComp.selectedLayers[0];
+
+    if (selected && !selected.source) { //check if selected layer is text layer
+        selected.sourceText.setValue(win.textBox.text);
+    } else {
+        textLayer = mainComp.layers.addText(win.textBox.text);
+    }
+
+}
