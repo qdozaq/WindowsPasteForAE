6656422d00888f4f31519329af7239e3689b7d80
qdozaq <doza@indiginis.com>
2017-08-31 21:04:55 -0400

    Resizable box, automatic text selection, see if [enter] works on windows

--- windowsPasteForAE.jsx
+++ windowsPasteForAE.jsx
@@ -5,10 +5,10 @@ Last Modified: 8/15/17
 todo:
 clean up code
 make dockable
-automatic text selection?
+√ - automatic text selection?
 replace text button with image button
 √ - auto fill text of selected text layer
-resizeable paste box
+√ - resizeable paste box
 make [enter] click highlighted button?
 */
 
@@ -18,38 +18,40 @@ var mainWin = new Window("palette", "paste", undefined);
 mainWin.btn = mainWin.add("Button", undefined, "Paste");
 
 mainWin.btn.onClick = function() {
-    // mainComp = app.project.activeItem;
-    // if (mainComp == null) {
-    //     // alert("please select a text layer");
-    //     return;
-    // }
-    // selected = mainComp.selectedLayers[0];
-    //
-    // if (selected && !selected.source)//check if selected layer is text layer
-    //     textWindow.box.text = selected.sourceText.value;
+    mainComp = app.project.activeItem;
+    if (mainComp == null) {
+        // alert("please select a text layer");
+        return;
+    }
+    selected = mainComp.selectedLayers[0];
+
+    if (selected && !selected.source)//check if selected layer is text layer
+        textWindow.box.text = selected.sourceText.value;
     textWindow.show();
 };
 
 //Text box that appears after click on mainWin.btn
-var textWindow = new Window("palette", "Paste Text", undefined, {resizeable: true});
+var textWindow = new Window("palette", "Paste Text", undefined, {resizeable: true, orientation:'column'});
 var h = textWindow.maximumSize.height / 2;
-// var w = textWindow.maximumSize.width / 2;
+// textWindow.minimumSize = [h,h]
 
-textWindow.box = textWindow.add("edittext", [
-    0, 0, h, h
-], "", {
+textWindow.box = textWindow.add("edittext", [0,0,h,h], "", {
     multiline: true,
     scrollable: true
 });
+textWindow.box.alignment=["fill", "fill"];
 
-textWindow.btns = textWindow.add("Group", undefined, undefined);
-var pasteBtn = textWindow.btns.add("Button", [0,0,h/2,30], "Paste");
-var cancelBtn = textWindow.btns.add("Button", [0,0,h/2,30], "Cancel");
+textWindow.btns = textWindow.add("Group", undefined, {orientation:'row'});
+textWindow.btns.alignment = 'center';
 
-textWindow.onResizing = function(){
-    textWindow.box.size = textWindow.size;
-    // pasteBtn.size = textWindow.size;
-    // cancelBtn.width = textWindow.width/2;
+var pasteBtn = textWindow.btns.add("Button", undefined, "Paste", {name:"ok"});
+var cancelBtn = textWindow.btns.add("Button", undefined, "Cancel");
+
+textWindow.box.active = true; //text box is highlighted
+
+var margin = 40;
+textWindow.onResizing = textWindow.onResize = function(){
+    this.layout.resize();
 }
 
 pasteBtn.onClick = function() {
